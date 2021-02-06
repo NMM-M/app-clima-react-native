c1b47d87a0cc4013466aa4f35d890b67d1312f3f
Nehemias Muñoz <Nehemias3712@gmail.com>
2021-02-06 17:02:52 -0300

    Enviando peticion api y leyendo resultados

--- App.js
+++ App.js
@@ -11,16 +11,28 @@ const App = () => {
   const [error,setError] = useState(false);
   const [message, setMessage] = useState('Hubo un error.');
   const [consultar, setConsultar] = useState(false);
+  const [resultado, setResultado] = useState({});
   const {city,country} = busqueda;
 
   //#endregion
   //#region USEEFFECT
     useEffect(() => {
-      if (consultar){
-        const appID = '5ad520fc7eaad50eb347a14d904a3a76'
-        const url = `api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${appID}`
-        console.log(url);
+      const consultarClima = async () =>{
+        if (consultar){
+          const appID = '5ad520fc7eaad50eb347a14d904a3a76'
+          const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${appID}`
+          try {
+            const respuesta = await fetch(url);
+            const resultado = await respuesta.json();
+            setResultado(resultado);
+            setConsultar(false);
+          } catch (error) {
+            setError(true);
+            setMessage('No tenemos datos para esta locación')
+          }
+        }
       }
+      consultarClima();
     },[consultar]);
   //#endregion
 
